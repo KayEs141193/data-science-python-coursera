9380f76e51932fd4fe7e3eaa25d91a45d7883193
KayEs141193 <kushal.saharan@gmail.com>
2019-02-24 18:48:31 +0530

    Added Collections in Python

--- course-1/Week1.py
+++ course-1/Week1.py
@@ -12,3 +12,22 @@ print(type(10.0))
 print(type('a'))
 print(None)
 print(adder)
+
+# Collection
+
+print("Collections in Python")
+
+# tuple
+# Immutable, ordered collection
+x = ( 1, 'a', 2, 'b')
+print(type(x))
+
+# list
+# Mutable, ordered collection
+x = [1,'a',2,'b']
+print(type(x))
+
+# dictionary
+# unordered, labelled collection
+x = { "Kushal": 23, "Sam": 22 }
+print(x)
