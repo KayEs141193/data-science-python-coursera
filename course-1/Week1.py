83c538eb2019a06656f4f677c631432f5bc6227e
KayEs141193 <kushal.saharan@gmail.com>
2019-02-24 18:25:24 +0530

    Added None and Function datatypes

--- course-1/Week1.py
+++ course-1/Week1.py
@@ -2,8 +2,13 @@
 
 print("DataTypes in Python")
 
+def adder(x,y):
+	return x + y
+
 print(type(10))
 print(type('abcd'))
 print(type(False))
 print(type(10.0))
 print(type('a'))
+print(None)
+print(adder)
