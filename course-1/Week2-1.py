1454a376ddd549acf42ce4708e53fc90a194f3ba
KayEs141193 <kushal.saharan@gmail.com>
2019-02-25 09:56:11 +0530

    Operations on Pandas Series

--- course-1/Week2-1.py
+++ course-1/Week2-1.py
@@ -29,5 +29,24 @@ s = pd.Series(sports)
 
 # Querying a Series
 
+# Both iloc and loc are attributes and not methods, therfore we use [] and not ()
 print(s.iloc[2])
 print(s.loc['Baseball'])
+
+s[1], s['Golf']
+
+# Use iloc and loc to avoid confusion
+# loc can also be used to add elements
+
+s = pd.Series([1,2,3,4,5])
+
+# np.sum(s) is faster than iterating over it, due to vectorization optimizations
+
+cricketLovers = pd.Series({'Cricket':'India',
+				 'Cricket':'Australia',
+				 'Basketball':'USA'})
+
+cricketLovers = pd.Series( ['India', 'Australia', 'USA'], index = ['Cricket', 'Cricket', 'Basketball'] )
+
+print(type(cricketLovers.loc['Cricket']))
+print(type(cricketLovers.loc['Basketball']))
