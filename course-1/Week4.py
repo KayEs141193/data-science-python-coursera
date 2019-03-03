faf795159e6b13fb9c385b9b1f01d6768684f0a6
KayEs141193 <kushal.saharan@gmail.com>
2019-03-03 15:24:51 +0530

    Added Distributions in Numpy

---
+++ course-1/Week4.py
@@ -0,0 +1,11 @@
+######### Distributions ############
+
+# Binomial Distribution: np.random.binomial(n,p,sims) # each sims provided number of success when n experiments are conducted
+# Uniform Distribution: np.random.uniform(0,1)
+# Normal Distribution: np.random.normal(mu,sigma,size=k)
+# Chi-squared Distribution: np.random.chisquare(deg_of_freedeom,size)
+# Bimodal Distribution: 
+
+# import scipy.stats as stats
+# stats.kurtosis(distribution_samples) # -ve flatter than normal, +ve peakier than normal
+# stats.skew
